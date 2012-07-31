1162941e5c68b37956630863463a1d4b5097dd10
Laura Kelly <llkelly88@gmail.com>
2012-07-30 19:46:29 -0700

    changed front page to 3 column gallery; began work on user profile page

--- settings.py
+++ settings.py
@@ -104,9 +104,6 @@ ROOT_URLCONF = 'idlhands.urls'
 
 TEMPLATE_DIRS = (
     "/home/norby/Dropbox/IDLHANDS/idlhands/idlhands_app/templates/"
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
 )
 
 INSTALLED_APPS = (
